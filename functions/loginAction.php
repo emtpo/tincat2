7711a92e1c7ce58a4a66dcb45557853bd3afea7d
emtpo <thepaut.emma@gmail.com>
2019-12-16 15:34:49 +0100

    correction4

--- functions/loginAction.php
+++ functions/loginAction.php
@@ -10,4 +10,12 @@ if( empty($_POST["pseudo"]) && empty($_POST["password"]) ){
 } else if( !empty($_POST["pseudo"]) && empty($_POST["password"]) ){
     $message = "Vous devez remplir un password";
     header("Location: ../login.php?message=$message");
-} 
+}
+if( !empty($_POST["pseudo"]) && !empty($_POST["password"]) ){
+    $req = $db->prepare("SELECT * FROM users WHERE pseudo = :pseudo AND password = :password");
+    $req->bindParam(":pseudo", $_POST["pseudo"]);
+    $req->bindParam(":password", $_POST["password"]);
+    $req->execute();
+    $result = $req->fetch(PDO::FETCH_ASSOC);
+    var_dump($result);
+}
