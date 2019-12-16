ae3491a99cc95aa97a0be08a39db388717a63ad0
emtpo <thepaut.emma@gmail.com>
2019-12-16 13:37:52 +0100

    setUser param

---
+++ functions/setUser.php
@@ -0,0 +1,35 @@
+<?php
+// Etape 1 : config database
+$DB_HOST = "localhost";
+$DB_NAME = "tincat";
+$DB_USER = "root";
+$DB_PASSWORD = "";
+// Etape 2 : Connexion to database
+try {
+    $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+var_dump($_POST);
+// Avant d'insérer en base de données faire les vérifications suivantes
+    // Vérifier si le pseudo ou le mot de passe est vide
+    // Ajouter un input confirm password et vérifier si les deux sont égaux
+    // Ajouter un champ email
+
+// SI email égal test@test.com et password égal admin
+if($_GET["emailUser"] == "test@test.com" && $_GET["password"] == "admin" && $_GET["password"] == "confirm-password" ){
+    echo "Hello " . $_GET["emailUser"];
+
+}else if( empty($_GET["password"]) ){
+   /******* Si password vide: tansmettre un message d'erreur, Header Location ....... ******/
+   header("Location: setUser.php?errorMessage=Empty password");
+}else{
+   header("Location: setUser.php?errorMessage=Erreur de connexion");
+}
+  
+// Etape 3 : prepare request
+$req = $db->prepare("INSERT INTO users (pseudo, password) VALUES(:pseudo, :password)");
+$req->bindParam(":pseudo", $_POST["pseudo"]);
+$req->bindParam(":password", $_POST["password"]);
+$req->execute();
--- register.php
+++ register.php
@@ -3,11 +3,26 @@
 <body>
     <div class="form-container">
         <h1>TINCAT</h1>
-        <form action="">
-            <input type="text" placeholder="pseudo">
-            <input type="password" placeholder="password">
-            <input type="submit" value="register">
+        <form action="functions/setUser.php" method="post">
+
+        <!-- <a href="_login.php?prenom=thomas&lastname=root">cliquez ici</a> -->
+           <input type="email" name="emailUser" placeholder="email">
+           <input type="text" placeholder="pseudo" name="pseudo">
+           <input type="password" placeholder="password" name="password">
+           <input type="password" placeholder="confirm password" name="confirm-password">
+           <input type="submit" value="register">
         </form>
     </div>
+    
+    <?php
+        /******* Afficher message d'erreur si il existe ******/
+        //var_dump($_GET);
+        if(isset($_GET["errorMessage"])){
+            echo "<div id=\"error\">";
+            echo $_GET["errorMessage"];
+            echo "</div>";
+        }
+    ?>
+
 </body>
 </html>
