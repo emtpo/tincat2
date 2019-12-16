7221a1813be34f71288f85e9f093c3889971a2a4
emtpo <thepaut.emma@gmail.com>
2019-12-16 17:35:20 +0100

    afficher utilisateur

--- functions/database.php
+++ functions/database.php
@@ -1,19 +1,11 @@
 <?php
-// Etape 1 : config database
 $DB_HOST = "localhost";
 $DB_NAME = "tincat";
 $DB_USER = "root";
 $DB_PASSWORD = "";
-// Etape 2 : Connexion to database
 try {
     $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
-}
-if (!empty($_POST["pseudo"]) && !empty($_POST["password"]) ){
-    $req = $db->prepare("SELECT * FROM users WHERE pseudo = :pseudo AND password = :password");
-    $req->blindParam(":pseudo", $_POST["pseudo"]);
-    $req->blindParam(":password", $_POST["password"]);
-    
 }
--- profils.php
+++ profils.php
@@ -5,10 +5,25 @@
     if(empty($_SESSION) ){
             header("Location: login.php");
     }
-        // rediriger vers login
 ?>
 
 <a href="functions/disconnect.php">Disconnect</a>
+
+<!-- Afficher les utilisateurs stocké dans la BDD sauf moi -->
+
+<!-- On récupère tout la data users -->
+<?php
+$reponse = $bdd->query('SELECT * FROM users');
+
+// On affiche chaque entrée une à une
+    while ($donnees = $reponse->fetch())
+    {
+        ?><p>
+        <strong>Utilisateurs :</strong> : <?php echo $donnees['pseudo']; ?><br />
+        <?php
+    }
+    $reponse->closeCursor(); // Termine le traitement de la requête
+    ?>
 
 <?php
 echo "Bonjour " . $_SESSION["pseudo"];
