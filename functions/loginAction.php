abc54b57668af7348c434c4e552b963f42709a7b
emtpo <thepaut.emma@gmail.com>
2019-12-16 15:29:40 +0100

    correction3

--- functions/loginAction.php
+++ functions/loginAction.php
@@ -1,3 +1,13 @@
 <?php
 require("database.php");
 
+if( empty($_POST["pseudo"]) && empty($_POST["password"]) ){
+    $message = "Vous devez remplir les deux champs";
+    header("Location: ../login.php?message=$message");
+} else if( empty($_POST["pseudo"]) && !empty($_POST["password"])  ){
+    $message = "Vous devez remplir un pseudo";
+    header("Location: ../login.php?message=$message");
+} else if( !empty($_POST["pseudo"]) && empty($_POST["password"]) ){
+    $message = "Vous devez remplir un password";
+    header("Location: ../login.php?message=$message");
+} 
